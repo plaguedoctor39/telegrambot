a358b7528a9464a48bf8be0622e1708819d3bfb1
daniil <shishovdanil@gmail.com>
2020-02-18 12:00:24 +0300

    tried to use yandex.metrica , thanks RKN. Added randomNumber game

--- bot.py
+++ bot.py
@@ -2,20 +2,44 @@
 import config
 import telebot
 import requests
+import json
+#import botan
+import random
+random.seed()
 s_city = "Moscow,RU"
 city_id = 524901
 appid = config.appid
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
              params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
+"""
+WEBHOOK_HOST = 'IP-адрес сервера, на котором запущен бот'
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (config.token)
+"""
 
 bot = telebot.TeleBot(config.token)
+"""
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('hello', 'tg', 'vkpage', 'weather')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+"""
+@bot.message_handler(commands=['random'])
+def cmd_random(message):
+    bot.send_message(message.chat.id, random.randint(1, 10))
+    # Если не нужно собирать ничего, кроме количества использований, замените третий аргумент message на None
+    #botan.track(config.botan_key, message.chat.id, message, 'Случайное число')
+    return
+
 
 """
 def request_current_weather(city_id):
